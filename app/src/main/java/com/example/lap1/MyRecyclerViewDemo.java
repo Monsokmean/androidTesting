f9609ced3c26a19e2e09b9efe03b2df7cdb6dac8
Mon Mat <monsokmeanmon@gmail.com>
2022-10-08 22:28:10 +0700

    Add New activity

---
+++ app/src/main/java/com/example/lap1/MyRecyclerViewDemo.java
@@ -0,0 +1,14 @@
+package com.example.lap1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyRecyclerViewDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_recycler_view_demo);
+    }
+}
